180cf1d352f62f0473363f5708e772f6c7b3222c
thrage10 <gareth.hill8@gmail.com>
2026-02-26 17:29:15 -0700

    Initial Commit

---
+++ SquallywoodNew/SquallywoodNewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SquallywoodNewApp.swift
+//  SquallywoodNew
+//
+//  Created by Gareth Hill on 2/26/26.
+//
+
+import SwiftUI
+
+@main
+struct SquallywoodNewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SquallywoodNewTests/SquallywoodNewTests.swift
@@ -0,0 +1,17 @@
+//
+//  SquallywoodNewTests.swift
+//  SquallywoodNewTests
+//
+//  Created by Gareth Hill on 2/26/26.
+//
+
+import Testing
+@testable import SquallywoodNew
+
+struct SquallywoodNewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
